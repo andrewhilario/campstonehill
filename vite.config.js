57f75db31f525955215f7610796e8f94bb0e3c6e
Drew <hilarioandrew12@gmail.com>
2023-12-11 21:35:50 +0800

    Add vite-plugin-css to package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import css from "vite-plugin-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    modules: false
+  }
 });
