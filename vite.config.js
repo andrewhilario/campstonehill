bdf7b4dd64c368bd9f582e726b074b097a859bed
Drew <hilarioandrew12@gmail.com>
2023-12-11 21:43:00 +0800

    Update font-face declarations and Vite CSS plugin configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import css from "vite-plugin-css";
+import ViteCssPlugin from "vite-plugin-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: false
+  build: {
+    assetsInlineLimit: 0
   }
 });
